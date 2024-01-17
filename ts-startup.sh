5e1453374925b72daeb2e8560980bab847ebe3a6
410-sh <85971872+410-sh@users.noreply.github.com>
2024-01-17 18:49:02 -0500

    Update ts-startup.sh
    added line to support headscale auth

--- ts-startup.sh
+++ ts-startup.sh
@@ -3,4 +3,8 @@ if ! command -v tailscale &> /dev/null
 then
         curl -fsSL https://tailscale.com/install.sh | sh
 fi
-sudo tailscale up --authkey=[YOUR_AUTH_KEY_HERE]
+# uncomment the following line if using Tailscale's server
+# sudo tailscale up --authkey=[YOUR_AUTH_KEY_HERE]
+
+# uncomment the following line if using a Headscale server
+# sudo tailscale up --login-server http://{SERVER-ADDR}:{PORT} --authkey=[YOUR_AUTH_KEY_HERE]
